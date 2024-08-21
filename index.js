62c08f6d8b6a78f50a8553df8ea715a0e3db500e
codonahoe <codonahoe@gmail.com>
2024-08-21 17:57:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mysql = require('mysql2');
+var cors = require('cors')
+var connection = mysql.createConnection({
+    host: 'ls-65984030458039fd3d922fc04d034a82be10f2be.czueisqkgcoa.us-east-1.rds.amazonaws.com',
+    user: 'dbmasteruser',
+    password: 'G~C$BJ5qfZ3r9SCQAhJ7%4ig2$fQ9osk',
+    database: 'dbmaster',
+})
+
+connection.connect(function(err){
+    if(err) throw err;
+    else console.log('Connection Successful')
+})
+
+var app = express();
+app.use(cors)
+app.use(express.json())
